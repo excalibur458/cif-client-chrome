46cecb10a9abbe212a12330bd4ef631b1f04b8c2
Tom Piscitell <tom.piscitell@gmail.com>
2013-09-03 09:49:49 -0400

    removed superfluous comment and fixed indentation

--- cif-browser-extension/content/settings.js
+++ cif-browser-extension/content/settings.js
@@ -126,7 +126,6 @@ CIF_CLIENT.test_settings=function(clickedbutton){
 	window.visitme="If you use a self-signed certificate, you will need to <a target='_blank' href='"+cifurl+cifquery+"?apikey="+cifapikey+"'>accept your certificate</a> before this will work.";
 	try{
 		$.getJSON(cifurl+"?q="+cifquery+"&apikey="+cifapikey+"&fmt=json",function(data) {
-			//This will never execute until CIF resturns valud JSON (as of v1 it does)
 			if (data['status']==200){
 				$(".teststatus",clickedbutton.parent().parent()).html('<span class="label label-success">connection successful</span>');
 			}
@@ -142,15 +141,15 @@ CIF_CLIENT.test_settings=function(clickedbutton){
 			} else if (e['status']==200){
 				/*
 				Workaround because CIFv1 does not return valid JSON and sets the Content-Type 
-				to application/json even when plain text is just returned. This causes $.getJSON()
-				and just plain old $.get() to fail everytime.
+				to application/json even when plain text is returned. This causes $.getJSON()
+				and $.get() to fail everytime.
 				*/
 				if (e['responseText']=='missing query') {
 					$(".teststatus",clickedbutton.parent().parent()).html('<span class="label label-success">connection successful</span>');
 					return;
 				} else {
 					console.log(e);
-				errmsg='bad response. is that the path to a CIF API?';
+					errmsg='bad response. is that the path to a CIF API?';
 				}
 			} else {
 				errmsg='Could not connect to that address.<br/><i>'+window.visitme+'</i>';
